3fabcf2d48eb4dbbfb532ea219e9201418bd8aa1
Alexander Koshelev <akoshelev@users.noreply.github.com>
2015-08-16 21:52:51 -0700

    completes demo3 example.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 'use strict';
 
-var gulp = require('gulp'),
-    through   = require('through'),
+var gulp    = require('gulp'),
+    concat  = require('gulp-concat'),
+    tap     = require('gulp-tap'),
+    uglify  = require('gulp-uglify'),
+    rename  = require('gulp-rename'),
     path    = require('path');
 
 function printFileInfo(file) {
@@ -9,12 +12,12 @@ function printFileInfo(file) {
     console.log('file:', path.basename(file.path), 'content-length: ', content.length);
 }
 
-gulp.task('read-js-content', function () {
-    gulp.src('src/app/**/*.js')
-        .pipe(through(printFileInfo));
-});
-
-gulp.task('read-js-no-content', function () {
-    gulp.src('src/app/**/*.js', { read: false })
-        .pipe(through(printFileInfo));
+gulp.task('default', function () {
+    return gulp.src('src/app/**/*.js')
+        .pipe(tap(printFileInfo))
+        .pipe(concat('scripts.js'))
+        .pipe(tap(printFileInfo))
+        .pipe(uglify())
+        .pipe(rename('scripts.min.js'))
+        .pipe(tap(printFileInfo));
 });
