be0668f8220c53585eaadfc0a7d23e0784c39ab2
Alexander Koshelev <akoshelev@users.noreply.github.com>
2015-08-16 21:00:30 -0700

    completes demo2

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,20 @@
 'use strict';
 
 var gulp = require('gulp'),
-    runSequence = require('run-sequence');
+    through   = require('through'),
+    path    = require('path');
 
-gulp.task('task1', function (cb) {
-    setTimeout(cb, 1000);
-    console.log('task1 started');
+function printFileInfo(file) {
+    var content = (file.contents || '').toString();
+    console.log('file:', path.basename(file.path), 'content-length: ', content.length);
+}
+
+gulp.task('read-js-content', function () {
+    gulp.src('src/app/**/*.js')
+        .pipe(through(printFileInfo));
 });
 
-gulp.task('task2', function (cb) {
-    setTimeout(cb, 1000);
-    console.log('task2 started');
-});
-
-gulp.task('parallel', ['task1', 'task2'], function () {
-    console.log('parallel task started.');
-});
-
-gulp.task('sequence', function (done) {
-    runSequence('task1', 'task2', done);
+gulp.task('read-js-no-content', function () {
+    gulp.src('src/app/**/*.js', { read: false })
+        .pipe(through(printFileInfo));
 });
